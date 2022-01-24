f3ddb1da2106c84064183440df00ae135728fc5e
SIPUN PATRA <spatra131099@gmail.com>
2022-01-24 18:23:22 +0530

    sipun

---
+++ MovieApp/app/src/main/java/com/example/movieapp/navigation/MovieNavigation.kt
@@ -0,0 +1,32 @@
+package com.example.movieapp.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.movieapp.screens.home.HomeScreen
+import com.example.movieapp.screens.details.DetailsScreen
+
+@ExperimentalAnimationApi
+@Composable
+fun MovieNavigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = MovieScreens.HomeScreen.name){
+
+        composable(MovieScreens.HomeScreen.name){
+            // here we pass where this should lead us to
+           HomeScreen(navController = navController)
+        }
+        composable(MovieScreens.DetailsScreen.name+"/{movie}",
+            arguments = listOf(navArgument(name = "movie"){type = NavType.StringType})){
+                backStackEntry ->
+
+            DetailsScreen(navController = navController,
+                backStackEntry.arguments?.getString("movie"))
+        }
+    }
+}
---
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,52 @@
+package com.example.movieapp.screens.home
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.navigation.MovieScreens
+import com.example.movieapp.widgets.MovieRow
+
+
+@ExperimentalAnimationApi
+@Composable
+fun HomeScreen(navController: NavController){
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Transparent,
+            elevation = 0.dp) {
+            Text(text = "Movie")
+        }
+    }) {
+       MainContent(navController = navController)
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+fun MainContent(
+    navController: NavController,
+
+    movieList: List<Movie> = getMovies()){
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+            MovieRow(movie = it){ movie ->
+                    navController.navigate(route = MovieScreens.DetailsScreen.name+"/$movie")
+
+                 //   Log.d("TAG", "MainContent: $movie")
+                }
+            }
+        }
+    }
+}
